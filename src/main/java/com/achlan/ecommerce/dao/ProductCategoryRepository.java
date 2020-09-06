5f0dbfe1f25fdab70f5284b7809ee1ba9c2af337
achlanbima <achlan_00011101@hotmail.com>
2020-09-06 20:47:30 +0700

    add CORS

--- src/main/java/com/achlan/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/achlan/ecommerce/dao/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.achlan.ecommerce.dao;
 import com.achlan.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/achlan/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/achlan/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package com.achlan.ecommerce.dao;
 
 import com.achlan.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
